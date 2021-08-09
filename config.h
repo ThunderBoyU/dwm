09b088298daffe82ee3094caf6cd561e134eca3b
Ukko <ukko@kotiboksi.xyz>
2021-08-09 20:19:12 +0300

    Fix spelling mistake

--- config.h
+++ config.h
@@ -8,8 +8,9 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */ static const char *fonts[]          = { "rissole:size=8" };
-static const char dmenufont[]       = "rissole:size=8";
+static const int topbar             = 1;        /* 0 means bottom bar */ 
+static const char *fonts[]          = { "monospace:size=9", "Joypixels:size=10" };
+static const char dmenufont[]       = "monospace:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
